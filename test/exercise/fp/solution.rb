988564fa79c808617c60cde3e9f21ae9626e9eeb
Павел Козырев <pavelkozyrev@MacBook-Air-Pavel.local>
2021-11-22 16:54:33 +0400

    add rating and chars_count methods

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,12 +4,29 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        filtered_array = array.select do |film|
+          !film['country'].nil? &&
+            film['country'].split(',').size >= 2 &&
+            !film['rating_kinopoisk'].nil? &&
+            !film['rating_kinopoisk'].to_f.zero?
+        end
+
+        total_rating = filtered_array.reduce(0) do |sum, film|
+          sum + film['rating_kinopoisk'].to_f
+        end
+
+        total_rating / filtered_array.size
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(films, threshold)
+        filtered_films = films.select do |film|
+          !film['rating_kinopoisk'].nil? &&
+            film['rating_kinopoisk'].to_f >= threshold
+        end
+        filtered_films.reduce(0) do |sum, film|
+          sum + film['name'].count('и')
+        end
       end
     end
   end
